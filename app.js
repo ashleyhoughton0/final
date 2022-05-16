3b2efa727311060340275561b0864352154c106b
ashleyhoughton0 <ahough1@artic.edu>
2022-05-16 10:55:05 -0500

    1

---
+++ app.js
@@ -0,0 +1,520 @@
+const cardArray = [
+  {
+    name: 'ahkopvnkv-ekvnv',
+    img:'images/tiles-01.png'
+  },
+  {
+    name:'avtmo',
+    img:'images/tiles-04.png',
+  },
+  {
+    name:'ayo',
+    img: 'images/tiles-06.png',
+  },
+  {
+    name:'cesse',
+    img:'images/tiles-10.png',
+  },
+  {
+    name:'cetto',
+    img:'images/tiles-12.png',
+  },
+  {
+    name: 'cvko',
+    img:'images/tiles-14.png',
+  },
+  {
+    name:'cokv',
+    img:'images/tiles-16.png',
+  },
+  {
+    name:'cokv hecv',
+    img: 'images/tiles-18.png',
+  },
+  {
+    name:'cokv tvlvme',
+    img:'images/tiles-22.png',
+  },
+  {
+    name:'cowatv',
+    img:'images/tiles-26.png',
+  },
+  {
+    name:'cufe',
+    img:'images/tiles-28.png',
+  },
+  {
+    name:'cufe enhompetv',
+    img:'images/tiles-30.png',
+  },
+  {
+    name:'cufunwv',
+    img:'images/tiles-32.png',
+  },
+  {
+    name:'cukuce',
+    img:'images/tiles-34.png',
+  },
+  {
+    name:'cukwv',
+    img:'images/tiles-36.png',
+  },
+  {
+    name:'culv',
+    img:'images/tiles-38.png',
+  },
+  {
+    name:'custake',
+    img:'images/tiles-40.png',
+  },
+  {
+    name:'cvmpv',
+    img:'images/tiles-42.png',
+  },
+  {
+    name:'cvse',
+    img:'images/tiles-44.png',
+  },
+  {
+    name:'cvstle',
+    img:'images/tiles-46.png',
+  },
+  {
+    name:'cvto',
+    img:'images/tiles-48.png',
+  },
+  {
+    name:'echaswv',
+    img:'images/tiles-50.png',
+  },
+  {
+    name:'eco',
+    img:'images/tiles-52.png',
+  },
+  {
+    name:'eco pihkv',
+    img:'images/tiles-54.png',
+  },
+  {
+    name:'efv',
+    img:'images/tiles-56.png',
+  },
+  {
+    name:'ekisse',
+    img:'images/tiles-58.png',
+  },
+  {
+    name:'ekvlpe',
+    img:'images/tiles-60.png',
+  },
+  {
+    name:'ekvnv',
+    img:'images/tiles-62.png',
+  },
+  {
+    name:'ele',
+    img:'images/tiles-64.png',
+  },
+  {
+    name:'enke',
+    img:'images/tiles-66.png',
+  },
+  {
+    name:'ero',
+    img:'images/tiles-68.png',
+  },
+  {
+    name:'esohotcickv',
+    img:'images/tiles-70.png',
+  },
+  {
+    name:'feke',
+    img:'images/tiles-72.png',
+  },
+  {
+    name:'fo',
+    img:'images/tiles-74.png',
+  },
+  {
+    name:'fuco',
+    img:'images/tiles-76.png',
+  },
+  {
+    name:'fune',
+    img:'images/tiles-78.png',
+  },
+  {
+    name:'fuswv',
+    img:'images/tiles-80.png',
+  },
+  {
+    name:'fvmecv',
+    img:'images/tiles-82.png',
+  },
+  {
+    name:'hakkv',
+    img:'images/tiles-84.png',
+  },
+  {
+    name:'hetute',
+    img:'images/tiles-86.png',
+  },
+  {
+    name:'honnv',
+    img:'images/tiles-88.png',
+  },
+  {
+    name:'hvcko',
+    img:'images/tiles-90.png',
+  },
+  {
+    name:'kapv',
+    img:'images/tiles-92.png',
+  },
+  {
+    name:'katcv',
+    img:'images/tiles-94.png',
+  },
+  {
+    name:'kute',
+    img:'images/tiles-96.png',
+  },
+  {
+    name:'locv',
+    img:'images/tiles-98.png',
+  },
+  {
+    name:'nokose',
+    img:'images/tiles-100.png',
+  },
+  {
+    name:'ohhompetv',
+    img:'images/tiles-102.png',
+  },
+  {
+    name:'ohliketv',
+    img:'images/tiles-104.png',
+  },
+  {
+    name:'perro',
+    img:'images/tiles-106.png',
+  },
+  {
+    name:'rakko',
+    img:'images/tiles-108.png',
+  },
+  {
+    name:'sesketv',
+    img:'images/tiles-110.png',
+  },
+  {
+    name:'toknawv',
+    img:'images/tiles-112.png',
+  },
+  {
+    name:'turwv',
+    img:'images/tiles-114.png',
+  },
+  {
+    name:'vhv cerehe',
+    img:'images/tiles-116.png',
+  },
+  {
+    name:'vsse',
+    img:'images/tiles-118.png',
+  },
+  {
+    name:'wakv',
+    img:'images/tiles-120.png',
+  },
+  {
+    name: 'ahkopvnkv-ekvnv',
+    img:'images/tiles-02.png'
+  },
+  {
+    name:'avtmo',
+    img:'images/tiles-03.png',
+  },
+  {
+    name:'ayo',
+    img: 'images/tiles-05.png',
+  },
+  {
+    name:'cesse',
+    img:'images/tiles-09.png',
+  },
+  {
+    name:'cetto',
+    img:'images/tiles-11.png',
+  },
+  {
+    name: 'cvko',
+    img:'images/tiles-13.png',
+  },
+  {
+    name:'cokv',
+    img:'images/tiles-15.png',
+  },
+  {
+    name:'cokv hecv',
+    img: 'images/tiles-17.png',
+  },
+  {
+    name:'cokv tvlvme',
+    img:'images/tiles-21.png',
+  },
+  {
+    name:'cowatv',
+    img:'images/tiles-25.png',
+  },
+  {
+    name:'cufe',
+    img:'images/tiles-27.png',
+  },
+  {
+    name:'cufe enhompetv',
+    img:'images/tiles-29.png',
+  },
+  {
+    name:'cufunwv',
+    img:'images/tiles-31.png',
+  },
+  {
+    name:'cukuce',
+    img:'images/tiles-33.png',
+  },
+  {
+    name:'cukwv',
+    img:'images/tiles-35.png',
+  },
+  {
+    name:'culv',
+    img:'images/tiles-37.png',
+  },
+  {
+    name:'custake',
+    img:'images/tiles-39.png',
+  },
+  {
+    name:'cvmpv',
+    img:'images/tiles-41.png',
+  },
+  {
+    name:'cvse',
+    img:'images/tiles-43.png',
+  },
+  {
+    name:'cvstle',
+    img:'images/tiles-45.png',
+  },
+  {
+    name:'cvto',
+    img:'images/tiles-47.png',
+  },
+  {
+    name:'echaswv',
+    img:'images/tiles-49.png',
+  },
+  {
+    name:'eco',
+    img:'images/tiles-51.png',
+  },
+  {
+    name:'eco pihkv',
+    img:'images/tiles-53.png',
+  },
+  {
+    name:'efv',
+    img:'images/tiles-55.png',
+  },
+  {
+    name:'ekisse',
+    img:'images/tiles-57.png',
+  },
+  {
+    name:'ekvlpe',
+    img:'images/tiles-59.png',
+  },
+  {
+    name:'ekvnv',
+    img:'images/tiles-61.png',
+  },
+  {
+    name:'ele',
+    img:'images/tiles-63.png',
+  },
+  {
+    name:'enke',
+    img:'images/tiles-65.png',
+  },
+  {
+    name:'ero',
+    img:'images/tiles-67.png',
+  },
+  {
+    name:'esohotcickv',
+    img:'images/tiles-69.png',
+  },
+  {
+    name:'feke',
+    img:'images/tiles-71.png',
+  },
+  {
+    name:'fo',
+    img:'images/tiles-73.png',
+  },
+  {
+    name:'fuco',
+    img:'images/tiles-75.png',
+  },
+  {
+    name:'fune',
+    img:'images/tiles-77.png',
+  },
+  {
+    name:'fuswv',
+    img:'images/tiles-79.png',
+  },
+  {
+    name:'fvmecv',
+    img:'images/tiles-81.png',
+  },
+  {
+    name:'hakkv',
+    img:'images/tiles-83.png',
+  },
+  {
+    name:'hetute',
+    img:'images/tiles-85.png',
+  },
+  {
+    name:'honnv',
+    img:'images/tiles-87.png',
+  },
+  {
+    name:'hvcko',
+    img:'images/tiles-89.png',
+  },
+  {
+    name:'kapv',
+    img:'images/tiles-91.png',
+  },
+  {
+    name:'katcv',
+    img:'images/tiles-93.png',
+  },
+  {
+    name:'kute',
+    img:'images/tiles-95.png',
+  },
+  {
+    name:'locv',
+    img:'images/tiles-97.png',
+  },
+  {
+    name:'nokose',
+    img:'images/tiles-99.png',
+  },
+  {
+    name:'ohhompetv',
+    img:'images/tiles-101.png',
+  },
+  {
+    name:'ohliketv',
+    img:'images/tiles-103.png',
+  },
+  {
+    name:'perro',
+    img:'images/tiles-105.png',
+  },
+  {
+    name:'rakko',
+    img:'images/tiles-107.png',
+  },
+  {
+    name:'sesketv',
+    img:'images/tiles-109.png',
+  },
+  {
+    name:'toknawv',
+    img:'images/tiles-111.png',
+  },
+  {
+    name:'turwv',
+    img:'images/tiles-113.png',
+  },
+  {
+    name:'vhv cerehe',
+    img:'images/tiles-115.png',
+  },
+  {
+    name:'vsse',
+    img:'images/tiles-117.png',
+  },
+  {
+    name:'wakv',
+    img:'images/tiles-119.png',
+  },
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
+
+function createBoard () {
+  for (let i = 0; i < cardArray.length; i++){
+    const card = document.createElement('img')
+    card.setAttribute('src','images/beginning-01.png')
+    card.setAttribute('data-id', i)
+    card.addEventListener('click', flipCard)
+    gridDisplay.appendChild(card)
+  }
+}
+createBoard()
+
+function checkMatch() {
+  const cards = document.querySelectorAll('#grid img')
+  const optionOneId = cardsChosenIds[0]
+  const optionTwoId = cardsChosenIds[1]
+  console.log(cards)
+  console.log('check for match!')
+  if (optionOneId == optionTwoId) {
+    cards[optionOneId].setAttribute('src', 'images/beginning-01.png')
+    cards[optionTwoId].setAttribute('src', 'images/beginning-01.png')
+    alert ('You clicked the same image!')
+  } else if (cardsChosen[0] == cardsChosen[1]){
+    alert('You found a match!')
+    cards[optionOneId].setAttribute('src', 'images/white.png')
+    cards[optionTwoId].setAttribute('src', 'images/white.png')
+    cards[optionOneId].removeEventListener('click', flipCard)
+    cards[optionTwoId].removeEventListener('click', flipCard)
+    cardsWon.push(cardsChosen)
+  } else {
+    cards[optionOneId].setAttribute('src', 'images/beginning-01.png')
+    cards[optionTwoId].setAttribute('src', 'images/beginning-01.png')
+    alert ('Sorry try again!')
+  }
+  resultDisplay.textContent = cardsWon.length
+  cardsChosen = []
+  cardsChosenIds = []
+
+  if (cardsWon.length == cardArray.length/2) {
+    resultDisplay.textContent = 'Congratulations you found them all!'
+  }
+}
+
+function flipCard() {
+  const cardId = this.getAttribute('data-id')
+  cardsChosen.push(cardArray[cardId].name)
+  cardsChosenIds.push(cardId)
+  console.log(cardsChosen)
+  console.log(cardsChosenIds)
+  this.setAttribute('src', cardArray[cardId].img)
+  if (cardsChosen.length === 2){
+    setTimeout(checkMatch, 500)
+  }
+}
